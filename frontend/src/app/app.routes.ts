f7a0bcb5d9d99a039f55fcc7db602b36640e2525
maurimnadal <maurim.nadal@gmail.com>
2025-05-30 13:18:57 -0300

    feat: adiciona pagina de registro do cliente com validações iniciais

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { RegistrarCliente } from './components/cliente/registrar-cliente/registrar-cliente';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path : "registrar-cliente", component : RegistrarCliente},
+];
