20671b73c427879e124f157f9058fa27be202412
maurimnadal <maurim.nadal@gmail.com>
2025-06-02 17:02:54 -0300

    feat: agora o cabeçalho está presente em todos os componentes da aplicação

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,24 +1,27 @@
 import { Routes } from '@angular/router';
-
+import { LayoutComponent } from './components/layout/layout.component';
+import { Home } from './components/home/home';
+import { RegistrarCliente } from './components/cliente/registrar-cliente/registrar-cliente';
+import { EditarCliente } from './components/cliente/editar-cliente/editar-cliente';
+import { ListarCliente } from './components/cliente/listar-cliente/listar-cliente';
+import { CriarContaComponent } from './components/conta/criar-conta/criar-conta';
 import { Depositar } from './components/depositar-sacar/depositar/depositar';
 import { Sacar } from './components/depositar-sacar/sacar/sacar';
-
 import { ListarTransacoes } from './components/transacoes/listar-transacoes/listar-transacoes';
-import { RegistrarCliente } from './components/cliente/registrar-cliente/registrar-cliente';
-import { ListarCliente } from './components/cliente/listar-cliente/listar-cliente';
-import { EditarCliente } from './components/cliente/editar-cliente/editar-cliente';
-import { CriarContaComponent } from './components/conta/criar-conta/criar-conta';
-import { Home } from './components/home/home';
 
 export const routes: Routes = [
-    { path : "registrar-cliente", component : RegistrarCliente},
-    { path: 'listar-cliente', component: ListarCliente },
-    { path: 'listar-transacoes', component: ListarTransacoes },
-    { path : "editar-cliente", component : EditarCliente},
-    { path: 'depositar', component: Depositar },
-    { path: 'sacar', component: Sacar },
-    { path: 'criar-conta', component: CriarContaComponent },
-    {path: "", component: Home}
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: Home },
+      { path: 'registrar-cliente', component: RegistrarCliente },
+      { path: 'editar-cliente', component: EditarCliente },
+      { path: 'listar-cliente', component: ListarCliente },
+      { path: 'criar-conta', component: CriarContaComponent },
+      { path: 'depositar', component: Depositar },
+      { path: 'sacar', component: Sacar },
+      { path: 'listar-transacoes', component: ListarTransacoes }
+    ]
+  }
 ];
-
-
