1a0d4252540cf6cb989a5f0060dd01e8516e0ce2
maurimnadal <maurim.nadal@gmail.com>
2025-06-02 17:12:11 -0300

    feat: agora a página de criar conta está centralizada e utiliza componentes material

--- frontend/src/app/components/conta/criar-conta/criar-conta.ts
+++ frontend/src/app/components/conta/criar-conta/criar-conta.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-criar-conta',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule
+  ],
   templateUrl: './criar-conta.html',
   styleUrls: ['./criar-conta.css']
 })
